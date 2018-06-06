7c243ab190260354bcedc9779207a01e38a6f508
Guilherme Bruzzi <guibruzzi@gmail.com>
2018-06-06 12:55:25 -0300

    Add getSelectedSla test

--- tests/sla.test.js
+++ tests/sla.test.js
@@ -1,9 +1,5 @@
-// import {
-//   baseLogisticsInfo,
-//   normalSla,
-// } from './mockGenerator'
+import { createLogisticsInfo, slas } from './mockGenerator'
 
-// import orderMock from './Order'
 import { getSelectedSla } from '../src/sla'
 
 describe('Sla', () => {
@@ -14,9 +10,46 @@ describe('Sla', () => {
       expect(selectedSla1).toBeNull()
       expect(selectedSla2).toBeNull()
     })
-    //
-    // it('should correct logisticInfo if valid selectedSla is passed', () => {
-    //   const selectedSla = ''
-    // })
+
+    it('should return correct logisticInfo if only itemIndex is passed', () => {
+      const logisticsInfo = createLogisticsInfo(
+        ['normalSla', 'expressSla', 'pickupSla'],
+        3
+      )
+
+      const expectedSelectedSla = slas.expressSla
+
+      const selectedSla = expectedSelectedSla.id
+
+      logisticsInfo[0].selectedSla = selectedSla
+
+      const newSelectedSla = getSelectedSla({
+        logisticsInfo,
+        itemIndex: 0,
+      })
+
+      expect(newSelectedSla).toEqual(expectedSelectedSla)
+    })
+
+    it('should return correct logisticInfo if valid selectedSla and itemIndex is passed', () => {
+      const logisticsInfo = createLogisticsInfo(
+        ['normalSla', 'expressSla', 'pickupSla'],
+        3
+      )
+
+      const expectedSelectedSla = slas.expressSla
+
+      const selectedSla = expectedSelectedSla.id
+
+      logisticsInfo[0].selectedSla = selectedSla
+
+      const newSelectedSla = getSelectedSla({
+        logisticsInfo,
+        selectedSla,
+        itemIndex: 0,
+      })
+
+      expect(newSelectedSla).toEqual(expectedSelectedSla)
+    })
   })
 })
